b6d08c7c716adba1dd27e0fd475ad6e2f3b34fcf
Nazmira syazwin <mirasyaz187@gmail.com>
2026-09-23 18:39:30 +0800

    Delete firebase_config.py

--- firebase_config.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-if not firebase_admin._apps:
-    # Ia akan baca fail JSON yang dimuat naik dalam GitHub secara terus
-    cred = credentials.Certificate(r"c:\python1\FarmPulse_API\farmpulse-76e0firebase-adminsdk-abc123.json")
-    firebase_admin.initialize_app(cred)
-
-db = firestore.client()
