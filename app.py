85ec41e5737f233e9c0a21e61532e74faccd103e
Nazmira syazwin <mirasyaz187@gmail.com>
2026-09-23 21:43:18 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.set_page_config(
 # CONFIGURATION
 # =========================================================
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://farmpulse-api-4.onrender.com"
 
 
 # =========================================================
