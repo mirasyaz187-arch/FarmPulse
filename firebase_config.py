1a1a2472e91f1a8e04dd94c0f98f158fea194043
Nazmira syazwin <mirasyaz187@gmail.com>
2026-09-23 18:45:38 +0800

    Refactor firebase_config.py initialization logic
    Refactor firebase initialization code for clarity.

--- firebase_config.py
+++ firebase_config.py
@@ -4,7 +4,9 @@ from firebase_admin import credentials, firestore
 
 if not firebase_admin._apps:
     firebase_secrets = dict(st.secrets["firebase"])
+
     cred = credentials.Certificate(firebase_secrets)
+
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
